d1925549a71b92142ac3ff5d82487410089c9413
Andrea Del Prete <andre.delprete@gmail.com>
2021-10-28 07:31:56 -0700

    [02-num-int] Move implementation of integration schemes to different file.

--- 02_optimal_control/numerical_integration.py
+++ 02_optimal_control/numerical_integration.py
@@ -8,8 +8,7 @@ Test different integration schemes and their derivatives.
 """
 
 import numpy as np
-import pinocchio as pin
-from numpy.linalg import norm, solve
+import solutions.numerical_integration_sol as sol
 
 class Integrator:
     ''' A class implementing different numerical integrator schemes '''
@@ -38,93 +37,37 @@ class Integrator:
         if(scheme=='RK-1'):
             for i in range(x.shape[0]-1):
                 ii = int(np.floor(i/ndt))
-                f = ode.f(x[i,:], U[ii,:], t[i])
-                dx[i,:] = f
-                x[i+1,:] = x[i,:] + h*f
+                x[i+1,:], dx[i,:] = sol.rk1(x[i,:], h, U[ii,:], t[i], ode)
                 t[i+1] = t[i] + h
         elif(scheme=='RK-2'):   # explicit midpoint method
             for i in range(x.shape[0]-1):
                 ii = int(np.floor(i/ndt))
-                x0 = x[i,:]
-                k1 = ode.f(x0,            U[ii,:], t[i])
-                k2 = ode.f(x0 + 0.5*h*k1, U[ii,:], t[i]+0.5*h)
-                dx[i,:] = k2
-                x[i+1,:] = x0 + h*k2
+                x[i+1,:], dx[i,:] = sol.rk2(x[i,:], h, U[ii,:], t[i], ode)
                 t[i+1] = t[i] + h
         elif(scheme=='RK-2-Heun'):
             for i in range(x.shape[0]-1):
                 ii = int(np.floor(i/ndt))
-                x0 = x[i,:]
-                k1 = ode.f(x0,        U[ii,:], t[i])
-                k2 = ode.f(x0 + h*k1, U[ii,:], t[i]+h)
-                dx[i,:] = 0.5*(k1+k2)
-                x[i+1,:] = x0 + h*dx[i,:]
+                x[i+1,:], dx[i,:] = sol.rk2heun(x[i,:], h, U[ii,:], t[i], ode)
                 t[i+1] = t[i] + h
         elif(scheme=='RK-3'): # Kutta's third-order method
             for i in range(x.shape[0]-1):
                 ii = int(np.floor(i/ndt))
-                x0 = x[i,:]
-                k1 = ode.f(x0,            U[ii,:], t[i])
-                k2 = ode.f(x0 + h/3*k1,   U[ii,:], t[i]+h/3)
-                k3 = ode.f(x0 + 2/3*h*k2, U[ii,:], t[i]+2*h/3)
-                dx[i,:] = (k1 + k2 + k3)/3.0
-#                k1 = ode.f(x0,                  U[ii,:], t[i])
-#                k2 = ode.f(x0 + h*0.5*k1,       U[ii,:], t[i]+0.5*h)
-#                k3 = ode.f(x0 + h*(-k1 + 2*k2), U[ii,:], t[i]+h)
-#                dx[i,:] = (k1 + 4*k2 + k3)/6.0
-                x[i+1,:] = x0 + h*dx[i,:]
-                t[i+1] = t[i] + h
+                x[i+1,:], dx[i,:] = sol.rk3(x[i,:], h, U[ii,:], t[i], ode)
+                t[i+1] = t[i] + h                
         elif(scheme=='RK-4'):
             for i in range(x.shape[0]-1):
                 ii = int(np.floor(i/ndt))
-                x0 = x[i,:]
-                k1 = ode.f(x0,            U[ii,:], t[i])
-                k2 = ode.f(x0 + 0.5*h*k1, U[ii,:], t[i]+0.5*h)
-                k3 = ode.f(x0 + 0.5*h*k2, U[ii,:], t[i]+0.5*h)
-                k4 = ode.f(x0 + h * k3,   U[ii,:], t[i]+h)
-                dx[i,:] = (k1 + 2*k2 + 2*k3 + k4)/6.0
-                x[i+1,:] = x0 + h*dx[i,:]
-                t[i+1] = t[i] + h
+                x[i+1,:], dx[i,:] = sol.rk4(x[i,:], h, U[ii,:], t[i], ode)
+                t[i+1] = t[i] + h                
         elif(scheme=='ImpEul'):
             for i in range(x.shape[0]-1):
                 ii = int(np.floor(i/ndt))
-                z = x[i,:] + h*ode.f(x[i,:], U[ii,:], t[i])
-                # Solve the following system of equations for z:
-                #   g(z) = z - x - h*f(z) = 0
-                # Start by computing the Newton step:
-                #   g(z) = g(z_i) + G(z_i)*dz = 0 => dz = -G(z_i)^-1 * g(z_i)
-                # where G is the Jacobian of g and z_i is our current guess of z
-                #   G(z) = I - h*F(z)
-                # where F(z) is the Jacobian of f wrt z.
-                I = np.identity(x_init.shape[0])
-                converged = False
-                for j in range(100):
-                    (f, Fx, Fu) = ode.f(z, U[ii,:], t[i], jacobian=True)
-                    g = z - x[i,:] - h*f
-#                    print(j, "|g|", norm(g))
-                    if(norm(g)<1e-8):
-                        converged = True
-#                        print(j, "|g|<thr", norm(g))
-                        break
-                    G = I - h*Fx
-                    dz = solve(G, -g)
-                    z += dz
-
-                if(not converged):
-                    print("Implicit Euler did not converge!!!! |g|=", norm(g))
-                    
-                dx[i,:] = f
-                x[i+1,:] = x[i,:] + h*dx[i,:]
+                x[i+1,:], dx[i,:] = sol.implicit_euler(x[i,:], h, U[ii,:], t[i], ode)
                 t[i+1] = t[i] + h
         if(scheme=='SemiImpEul'):
-            nv = int(x.shape[0]/2)
             for i in range(x.shape[0]-1):
                 ii = int(np.floor(i/ndt))
-                f = ode.f(x[i,:], U[ii,:], t[i])
-                v_next = x[i,-nv:] + h*f[-nv:]
-                dx[i,:] = f
-                dx[i,:nv] = v_next
-                x[i+1,:] = x[i,:] + h*f
+                x[i+1,:], dx[i,:] = sol.semi_implicit_euler(x[i,:], h, U[ii,:], t[i], ode)
                 t[i+1] = t[i] + h
             
         self.dx = dx
@@ -154,52 +97,18 @@ class Integrator:
         x[0,:] = x_init
         t[0] = t_init
         
-        I = np.identity(nx)
         if(scheme=='RK-1'):
             for i in range(N):
-                (f, f_x, f_u) = ode.f(x[i,:], U[i,:], t[i], jacobian=True)
-                dx[i,:] = f
-                x[i+1,:] = x[i,:] + h*f
+                x[i+1,:], dx[i,:], phi_x, phi_u = sol.rk1(x[i,:], h, U[i,:], t[i], ode, True)
                 t[i+1] = t[i] + h
-                
-                phi_x = I + h*f_x
-                phi_u = h * f_u
                 ix, ix1, ix2 = i*nx, (i+1)*nx, (i+2)*nx
                 iu, iu1 = i*nu, (i+1)*nu
                 dXdU[ix1:ix2,:] = phi_x.dot(dXdU[ix:ix1,:]) 
                 dXdU[ix1:ix2,iu:iu1] += phi_u
         elif(scheme=='RK-4'):
             for i in range(x.shape[0]-1):
-                x1 = x[i,:]
-                t1 = t[i]
-                (k1, f1_x, f1_u) = ode.f(x1, U[i,:], t1, jacobian=True)
-                k1_x = f1_x
-                k1_u = f1_u
-                
-                x2 = x1 + 0.5*h*k1
-                t2 = t[i]+0.5*h
-                (k2, f2_x, f2_u) = ode.f(x2, U[i,:], t2, jacobian=True)
-                k2_x = f2_x.dot(I + 0.5*h*k1_x)
-                k2_u = f2_u + 0.5*h*f2_x @ k1_u
-                
-                x3 = x1 + 0.5*h*k2
-                t3 = t[i]+0.5*h
-                (k3, f3_x, f3_u) = ode.f(x3, U[i,:], t3, jacobian=True)
-                k3_x = f3_x.dot(I + 0.5*h*k2_x)
-                k3_u = f3_u + 0.5*h*f3_x @ k2_u
-                
-                x4 = x1 + h * k3
-                t4 = t[i]+h
-                (k4, f4_x, f4_u) = ode.f(x4, U[i,:], t4, jacobian=True)
-                k4_x = f4_x.dot(I + h*k3_x)
-                k4_u = f4_u + h*f4_x @ k3_u
-                
-                dx[i,:] = (k1 + 2*k2 + 2*k3 + k4)/6.0
-                x[i+1,:] = x1 + h*dx[i,:]
+                x[i+1,:], dx[i,:], phi_x, phi_u = sol.rk4(x[i,:], h, U[i,:], t[i], ode, True)
                 t[i+1] = t[i] + h
-                
-                phi_x = I + h*(k1_x + 2*k2_x + 2*k3_x + k4_x)/6.0
-                phi_u =     h*(k1_u + 2*k2_u + 2*k3_u + k4_u)/6.0
                 ix, ix1, ix2 = i*nx, (i+1)*nx, (i+2)*nx
                 iu, iu1 = i*nu, (i+1)*nu
                 dXdU[ix1:ix2,:] = phi_x.dot(dXdU[ix:ix1,:]) 
@@ -234,31 +143,26 @@ class Integrator:
                 print("dXdU", dXdU)
                 print("dXdU_fd", dXdU_fd)
                 
-            
 
-        
-    
 if __name__=='__main__':
     import matplotlib.pyplot as plt
-    import arc.utils.plot_utils as plut
+    import orc.utils.plot_utils as plut
 #    import time
     from ode import ODERobot, ODELinear, ODESin, ODEStiffDiehl
-    from arc.utils.robot_loaders import loadUR
-    from arc.utils.robot_wrapper import RobotWrapper
-    from arc.utils.robot_simulator import RobotSimulator
+    from orc.utils.robot_loaders import loadUR
+    from orc.utils.robot_wrapper import RobotWrapper
+    from orc.utils.robot_simulator import RobotSimulator
     import numerical_integration_conf as conf
     np.set_printoptions(precision=3, suppress=True);
     
-    ''' Test DDP with a manipulator
+    ''' Test numerical integration with a manipulator
     '''
     print("".center(conf.LINE_WIDTH,'#'))
     print(" Numerical integration ".center(conf.LINE_WIDTH, '#'))
     print("".center(conf.LINE_WIDTH,'#'), '\n')
 
     N = int(conf.T/conf.dt);                 # horizon size
-#    N=10
     dt = conf.dt;               # time step
-#    dt = 0.01
     DEBUG = False;
     PLOT_STUFF = 1
     linestyles = ['-', '--', ':', '-.']
@@ -270,17 +174,14 @@ if __name__=='__main__':
     # choose the number of inner steps so that the number of function evaluations
     # is the same for every method
     integrators = []
-    integrators += [{'scheme': 'RK-4',      'ndt': 1000}]    # used as ground truth
+    integrators += [{'scheme': 'RK-3',      'ndt': 1000}]    # used as ground truth
     integrators += [{'scheme': 'RK-1',      'ndt': 12}]
     integrators += [{'scheme': 'RK-2',      'ndt': 6}]
-#    integrators += [{'scheme': 'RK-2-Heun', 'ndt': 6}]
     integrators += [{'scheme': 'RK-3',      'ndt': 4}]
-    integrators += [{'scheme': 'RK-4',      'ndt': 3}]
-#    integrators += [{'scheme': 'ImpEul',      'ndt': 12}]
+#    integrators += [{'scheme': 'RK-4',      'ndt': 3}]
     
 #    integrators += [{'scheme': 'RK-1',      'ndt': 10}]
 #    integrators += [{'scheme': 'RK-2',      'ndt': 10}]
-##    integrators += [{'scheme': 'RK-2-Heun', 'ndt': 10}]
 #    integrators += [{'scheme': 'RK-3',      'ndt': 10}]
 #    integrators += [{'scheme': 'RK-4',      'ndt': 10}]
         
