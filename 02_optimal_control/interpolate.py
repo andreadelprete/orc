[02] Add new cost on joint accelerations. Add gripper frame to UR5 robot model. Move computation of joint viscous friction inside RobotWrapper class (instead of ODE class). Fix table height in viewer. Extend interpolate script to merge several trajectories into one.

02_optimal_control/interpolate.py
@@ -10,33 +10,50 @@ import single_shooting_conf as conf
 import orc.utils.plot_utils as plut
 
 dt_robot = 0.001
-DATA_FILE_NAME = 'ur5_q_ref'
 
-data = np.load(conf.DATA_FILE_NAME+'.npz') # , q=X[:,:nq], v=X[:,nv:], u=U
-q_in = data['q']
-N_in = q_in.shape[0]
-nq = q_in.shape[1]
+INPUT_FILE_NAMES = ['home_2_table', 'table_2_belt']
+#INPUT_FILE_NAME = 'table_2_belt'
+#INPUT_FILE_NAME = 'belt_2_home'
 
+OUTPUT_FINE_NAME = 'home_2_table_2_belt'
+
+q_in = []
+N_out = 0
 ratio = int(conf.dt/dt_robot)
-N_out = 1 + (N_in-1)*ratio
+for file_name in INPUT_FILE_NAMES:
+    data = np.load(file_name+'.npz') # , q=X[:,:nq], v=X[:,nv:], u=U
+    q_in.append(data['q'])
+    N_in = q_in[-1].shape[0]
+    N_out += 1 + (N_in-1)*ratio
+    
+nq = q_in[-1].shape[1]
 q_out = np.empty((N_out, nq))
-for i in range(N_in-1):
-    for j in range(ratio):
-        q_out[i*ratio+j,:] = ((ratio-j)*q_in[i,:] + j*q_in[i+1,:]) / ratio
-q_out[-1,:] = q_in[-1,:]
+index = 0
+for k in range(len(INPUT_FILE_NAMES)):
+    N_in = q_in[k].shape[0]
+    for i in range(N_in-1):
+        for j in range(ratio):
+            q_out[index,:] = ((ratio-j)*q_in[k][i,:] + j*q_in[k][i+1,:]) / ratio
+            index += 1
+    q_out[index,:] = q_in[k][-1,:]
+    index += 1
 
 # PLOT STUFF
-time_in  = np.arange(0.0, N_in*conf.dt, conf.dt)[:N_in]
 time_out = np.arange(0.0, N_out*dt_robot, dt_robot)[:N_out]
+T0 = 0.0
 
 (f, ax) = plut.create_empty_figure(int(nq/2),2)
 ax = ax.reshape(nq)
-for i in range(nq):
-    ax[i].plot(time_in,  q_in[:,i],  label='q_in', alpha=0.7)
-    ax[i].plot(time_out, q_out[:,i], '--', label='q_out', alpha=0.7)
-    ax[i].set_xlabel('Time [s]')
-    ax[i].set_ylabel(r'$q_'+str(i)+'$ [rad]')
-    leg = ax[i].legend()
+for k in range(len(INPUT_FILE_NAMES)):
+    T1 = T0 + q_in[k].shape[0]*conf.dt
+    time_in  = np.arange(T0, T1, conf.dt)[:q_in[k].shape[0]]
+    T0 = T1
+    for i in range(nq):
+        ax[i].plot(time_in,  q_in[k][:,i],  label='q_in', alpha=0.7)
+        ax[i].plot(time_out, q_out[:,i], '--', label='q_out', alpha=0.7)
+        ax[i].set_xlabel('Time [s]')
+        ax[i].set_ylabel(r'$q_'+str(i)+'$ [rad]')
+    leg = ax[-1].legend()
     leg.get_frame().set_alpha(0.5)
 
-np.savez_compressed(DATA_FILE_NAME, q=q_out)
+np.savez_compressed(OUTPUT_FINE_NAME+'_robot', q=q_out)

02_optimal_control/ode.py
@@ -74,7 +74,7 @@ class ODERobot:
     ''' An ordinary differential equation representing a robotic system
     '''
     
-    def __init__(self, name, robot, B=None):
+    def __init__(self, name, robot):
         ''' robot: instance of RobotWrapper
         '''
         self.name = name
@@ -86,11 +86,7 @@ class ODERobot:
         self.Fx = np.zeros((self.nx, self.nx))
         self.Fx[:nv, nv:] = np.identity(nv)
         self.Fu = np.zeros((self.nx, self.nu))
-        self.dx = np.zeros(2*nv)
-        self.B = np.zeros((nv,nv))
-        if(B is not None):
-            self.B = np.diagflat(B)
-            
+        self.dx = np.zeros(2*nv)            
         
         
     ''' System dynamics '''
@@ -107,18 +103,19 @@ class ODERobot:
             pin.computeAllTerms(model, data, q, v)
             ddq = (u-data.nle) / data.M[0]
         else:
-            ddq = pin.aba(model, data, q, v, u) - self.B @ v
+            ddq = self.robot.aba(q, v, u) #pin.aba(model, data, q, v, u) - self.B @ v
 
         self.dx[:nv] = v
         self.dx[nv:] = ddq
         
         if(jacobian):
-            pin.computeABADerivatives(model, data, q, v, u)
+            ddq_dq, ddq_dv, ddq_du = self.robot.abaDerivatives(q, v, u)
+#            pin.computeABADerivatives(model, data, q, v, u)
             self.Fx[:nv, :nv] = 0.0
             self.Fx[:nv, nv:] = np.identity(nv)
-            self.Fx[nv:, :nv] = data.ddq_dq
-            self.Fx[nv:, nv:] = data.ddq_dv - self.B
-            self.Fu[nv:, :] = data.Minv
+            self.Fx[nv:, :nv] = ddq_dq
+            self.Fx[nv:, nv:] = ddq_dv
+            self.Fu[nv:, :] = ddq_du
             
             return (np.copy(self.dx), np.copy(self.Fx), np.copy(self.Fu))
         

02_optimal_control/single_shooting_conf.py
@@ -11,24 +11,31 @@ from math import sqrt
 np.set_printoptions(precision=3, linewidth=200, suppress=True)
 LINE_WIDTH = 60
 
-T = 2.0                         # OCP horizon
+T = 3.0                         # OCP horizon
 dt = 0.02                     # OCP time step
 integration_scheme = 'RK-1'
 use_finite_diff = False
 
-DATA_FILE_NAME = 'ur5_X_U_optimal'
+#DATA_FILE_NAME = 'home_2_table'
+DATA_FILE_NAME = 'table_2_belt'
+#DATA_FILE_NAME = 'belt_2_home'
+
+INITIAL_GUESS_FILE = None # use None if you don't have an initial guess
+#INITIAL_GUESS_FILE = 'home_2_table' 
+INITIAL_GUESS_FILE = 'table_2_belt'
 
 #system = 'ur'
 system = 'ur-lab'
 #system='double-pendulum'
 #system='pendulum'
 
-weight_final_pos = 0   # cost function weight for final end-effector position
-weight_final_vel = 0   # cost function weight for final end-effector velocity
-weight_final_q  = 10   # cost function weight for final joint positions
-weight_final_dq = 10   # cost function weight for final joint velocities
-weight_dq = 1e-1       # cost function weight for joint velocities
-weight_u = 1e-4        # cost function weight for joint torques
+weight_final_pos = 0    # cost function weight for final end-effector position
+weight_final_vel = 0    # cost function weight for final end-effector velocity
+weight_final_q  = 100   # cost function weight for final joint positions
+weight_final_dq = 100   # cost function weight for final joint velocities
+weight_dq  = 1e-1       # cost function weight for joint velocities
+weight_ddq = 1e-2       # cost function weight for joint accelerations
+weight_u   = 0e-2       # cost function weight for joint torques
 
 if(system=='ur'):
     nq = 6
@@ -43,7 +50,8 @@ elif(system=='ur-lab'):
     fixed_world_translation = np.array([0.5, 0.35, 1.75])
     frame_name = 'tool0'
     # list of frames that should not collide with table, and their minimum distance
-    table_collision_frames = [('tool0', 0.07),
+    table_collision_frames = [('gripper', 0.04),
+                              ('tool0', 0.07),
                               ('wrist_1_joint', 0.07),
                               ('wrist_2_joint', 0.07)]
     # list of frame pairs that should not collide together
@@ -57,8 +65,8 @@ elif(system=='ur-lab'):
     q_table  = np.array([-0.176686, -1.049217, -1.886168, -1.754567, -1.522096, -1.082379])
     q_belt_2 = np.array([-3.313833, -2.238478, -1.068578, -1.395336, -1.554650, -0.658182])
     
-    q_0 = q_home
-    q_des = q_table
+    q0 = q_table
+    q_des = q_belt_2
     
     p_des = np.array([0.5, 0.4, 1.05])   # desired end-effector final position
     B = np.array([10., 10., 10., 5., 1., 1.]) # joint viscous friction coefficient

02_optimal_control/single_shooting_problem.py
@@ -46,18 +46,22 @@ class SingleShootingProblem:
     def add_running_cost(self, c, weight=1):
         self.running_costs += [(weight,c)]
         self.last_values.__dict__[c.name] = 0.0
+        self.last_values.__dict__[c.name+'_grad'] = 0.0
     
     def add_final_cost(self, c, weight=1):
         self.final_costs += [(weight,c)]
         self.last_values.__dict__[c.name] = 0.0
+        self.last_values.__dict__[c.name+'_grad'] = 0.0
         
     def add_path_ineq(self, c):
         self.path_ineqs += [c]
         self.last_values.__dict__[c.name] = 0.0
+        self.last_values.__dict__[c.name+'_grad'] = 0.0
     
     def add_final_ineq(self, c):
         self.final_ineqs += [c]
         self.last_values.__dict__[c.name] = 0.0
+        self.last_values.__dict__[c.name+'_grad'] = 0.0
         
     '''*************************************************'''
     '''                 COST FUNCTIONS                  '''
@@ -90,6 +94,7 @@ class SingleShootingProblem:
         # reset the variables storing the costs
         for (w,c) in self.running_costs:
             self.last_values.__dict__[c.name] = 0.0
+            self.last_values.__dict__[c.name+'_grad'] = 0.0
             
         for i in range(U.shape[0]):
             for (w,c) in self.running_costs:
@@ -100,6 +105,7 @@ class SingleShootingProblem:
                 cost += w * self.dt * ci
                 grad += w * self.dt * dci
                 self.last_values.__dict__[c.name] += w * self.dt * ci
+                self.last_values.__dict__[c.name+'_grad'] += norm(w * self.dt * dci)
             t += self.dt
         return (cost, grad)
         
@@ -124,6 +130,7 @@ class SingleShootingProblem:
             cost += w * ci
             grad += w * dci
             self.last_values.__dict__[c.name] = w * ci
+            self.last_values.__dict__[c.name+'_grad'] = norm(w * dci)
         return (cost, grad)
         
     
@@ -183,6 +190,7 @@ class SingleShootingProblem:
         self.last_values.cost = cost  
         self.last_values.running_cost = run_cost
         self.last_values.final_cost = fin_cost
+        self.last_values.grad = norm(grad)
         return (cost, grad)
     
     '''*************************************************'''
@@ -351,15 +359,15 @@ class SingleShootingProblem:
         
         
     def clbk(self, xk):
-        print('Iter %3d, cost %5f'%(self.iter, self.last_values.cost))
+        print('Iter %3d, cost %7.3f, grad %7.3f'%(self.iter, self.last_values.cost, self.last_values.grad))
         for (w,c) in self.running_costs:
-            print("\t Running cost %40s: %9.3f"%(c.name, self.last_values.__dict__[c.name]))
+            print("\t Running cost %40s: %7.3f %7.3f"%(c.name, self.last_values.__dict__[c.name], self.last_values.__dict__[c.name+'_grad']))
         for (w,c) in self.final_costs:
-            print("\t Final cost   %40s: %9.3f"%(c.name, self.last_values.__dict__[c.name]))
+            print("\t Final cost   %40s: %7.3f %7.3f"%(c.name, self.last_values.__dict__[c.name], self.last_values.__dict__[c.name+'_grad']))
         for c in self.path_ineqs:
-            print('\t Path ineq    %40s: %9.3f'%(c.name, np.min(self.last_values.__dict__[c.name])))
+            print('\t Path ineq    %40s: %7.3f'%(c.name, np.min(self.last_values.__dict__[c.name])))
         for c in self.final_ineqs:
-            print('\t Final ineq   %40s: %9.3f'%(c.name, np.min(self.last_values.__dict__[c.name])))
+            print('\t Final ineq   %40s: %7.3f'%(c.name, np.min(self.last_values.__dict__[c.name])))
 #        print('\t\tlast u:', self.U.T)
         self.iter += 1
         if(self.iter%10==0):

02_optimal_control/test_single_shooting.py
@@ -20,7 +20,7 @@ import single_shooting_conf as conf
 from ode import ODERobot
 from numerical_integration import Integrator
 from cost_functions import OCPFinalCostState, OCPFinalCostFramePos, OCPFinalCostFrame
-from cost_functions import OCPRunningCostQuadraticJointVel, OCPRunningCostQuadraticControl
+from cost_functions import OCPRunningCostQuadraticJointVel, OCPRunningCostQuadraticJointAcc, OCPRunningCostQuadraticControl
 from inequality_constraints import OCPFinalPlaneCollisionAvoidance, OCPPathPlaneCollisionAvoidance
 from inequality_constraints import OCPFinalJointBounds, OCPPathJointBounds
 from inequality_constraints import OCPFinalSelfCollisionAvoidance, OCPPathSelfCollisionAvoidance
@@ -42,15 +42,20 @@ elif(system=='double-pendulum'):
 elif(system=='pendulum'):
     r = loadPendulum()
 robot = RobotWrapper(r.model, r.collision_model, r.visual_model, fixed_world_translation=conf.fixed_world_translation)
+robot.setJointViscousFriction(conf.B)
 nq, nv = robot.nq, robot.nv    
 n = nq+nv                       # state size
 m = robot.na                    # control size
 
 # compute initial guess for control inputs
 U = np.zeros((N,m))           
-u0 = robot.gravity(conf.q0)
-for i in range(N):
-    U[i,:] = u0
+if(conf.INITIAL_GUESS_FILE is None):
+    u0 = robot.gravity(conf.q0)
+    for i in range(N):
+        U[i,:] = u0
+else:
+    data = np.load(conf.INITIAL_GUESS_FILE+'.npz') # , q=X[:,:nq], v=X[:,nv:], u=U
+    U = data['u']
 
 # create simulator 
 simu = RobotSimulator(conf, robot)
@@ -72,7 +77,7 @@ for (frame1, frame2, d) in conf.self_collision_frames:
     simu.add_frame_axes(frame2, radius=d, length=0.0, color=(1.,0,0,0.2))
 
 # create OCP
-ode = ODERobot('ode', robot, conf.B)
+ode = ODERobot('ode', robot)
 problem = SingleShootingProblem('ssp', ode, conf.x0, dt, N, conf.integration_scheme, simu)
 
 # simulate motion with initial guess    
@@ -101,6 +106,10 @@ if(conf.weight_dq>0):
     dq_cost = OCPRunningCostQuadraticJointVel("joint vel", robot)
     problem.add_running_cost(dq_cost, conf.weight_dq)    
 
+if(conf.weight_ddq>0):
+    ddq_cost = OCPRunningCostQuadraticJointAcc("joint acc", robot)
+    problem.add_running_cost(ddq_cost, conf.weight_ddq)    
+
 ''' Create constraints '''
 q_min = robot.model.lowerPositionLimit
 q_max = robot.model.upperPositionLimit
@@ -115,11 +124,11 @@ problem.add_final_ineq(joint_bounds_final)
 # inequalities for avoiding collisions with the table
 for (frame, dist) in conf.table_collision_frames:
     table_avoidance = OCPPathPlaneCollisionAvoidance("table col "+frame, robot, frame, 
-                                                     lab.table_normal, lab.table_pos[2]+dist)
+                                                     lab.table_normal, lab.table_pos[2]+0.5*lab.table_size[2]+dist)
     problem.add_path_ineq(table_avoidance)
     
     table_avoidance = OCPFinalPlaneCollisionAvoidance("table col fin "+frame, robot, frame, 
-                                                     lab.table_normal, lab.table_pos[2]+dist)
+                                                     lab.table_normal, lab.table_pos[2]+0.5*lab.table_size[2]+dist)
     problem.add_final_ineq(table_avoidance)
 
 # inequalities for avoiding self-collisions
@@ -143,7 +152,8 @@ problem.solve(y0=U.reshape(N*m), use_finite_diff=conf.use_finite_diff)
 
 X, U = problem.X, problem.U
 print('U norm:', norm(U))
-print('X_N\n', X[-1,:].T)
+print('q_N-q_des\n', X[-1,:nq]-conf.q_des)
+print('dq_N\n', X[-1,nq:])
 
 # display final optimized motion
 print("Showing final motion in viewer")

utils/lab_utils.py
@@ -8,7 +8,7 @@ Created on Thu Jul 28 11:45:47 2022
 import numpy as np
 
 table_normal = np.array([0., 0., 1.])
-table_pos = np.array([0.5, 0.4, 0.85]) #-0.7
+table_pos = np.array([0.5, 0.4, 0.83]) #-0.7
 table_size = np.array([1.5, 1.5, 0.04])
 table_color = (0.5, 0.5, 0.5, 1.)
 

utils/robot_wrapper.py
@@ -16,6 +16,7 @@ class RobotWrapper(PinocchioRobotWrapper):
     
     def __init__(self, model = pin.Model(), collision_model = None, visual_model = None, verbose=False, fixed_world_translation=None):
         super().__init__(model, collision_model, visual_model, verbose)
+        self.B = np.zeros((model.nv, model.nv))
         if(fixed_world_translation is None):
             self.w2b = np.zeros(3) # world to base fixed translation
         else:
@@ -26,6 +27,12 @@ class RobotWrapper(PinocchioRobotWrapper):
         if(self.model.joints[0].nq==7):
             return self.model.nv-6
         return self.model.nv
+    
+    def setJointViscousFriction(self, friction_coefficients):
+        assert(friction_coefficients.shape[0]==self.model.nv)
+#        self.model.friction = B # do not work
+#        self.model.damping = B  # do not work
+        self.B = np.diagflat(friction_coefficients)
     
     def setFixedWorldTranslation(self, translation):
         ''' Set a fixed translation from the world frame to the universe frame of the robot URDF '''
@@ -146,8 +153,15 @@ class RobotWrapper(PinocchioRobotWrapper):
 
     def nle(self, q, v, update=True):
         if(update):
-            return pin.nonLinearEffects(self.model, self.data, q, v)
-        return self.data.nle
+            pin.nonLinearEffects(self.model, self.data, q, v)
+        return self.data.nle - self.B @ v
+    
+    def aba(self, q, v, u):
+        return pin.aba(self.model, self.data, q, v, u) - self.B @ v
+    
+    def abaDerivatives(self, q, v, u):
+        pin.computeABADerivatives(self.model, self.data, q, v, u)
+        return self.data.ddq_dq, self.data.ddq_dv - self.B, self.data.Minv
         
     def com(self, q=None, v=None, a=None, update=True):
         if(update==False or q is None):