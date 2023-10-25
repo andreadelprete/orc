2f1f0de161fa666065ee02bbf0baa265ddf0a890
andreadelprete <andre.delprete@gmail.com>
2023-10-25 13:15:34 +0200

    Add viewer-independent method to display DISI lab in viewer

--- utils/lab_utils.py
+++ utils/lab_utils.py
@@ -7,22 +7,44 @@ Created on Thu Jul 28 11:45:47 2022
 """
 import numpy as np
 import os
+from orc.utils.viz_utils import addViewerBox, applyViewerConfiguration
 
 fixed_world_translation = np.array([0.5, 0.35, 1.75])
 LOCOSIM_DIR = os.getenv('LOCOSIM_DIR', '/home/adelprete/devel/src/locosim')
 TABLE_STL_FILE = LOCOSIM_DIR + '/ros_impedance_controller/worlds/models/tavolo/mesh/tavolo.stl'
-table_stl_pos = np.array([0.0, 0.0, -0.02])
 
 table_normal = np.array([0., 0., 1.])
-table_pos = np.array([0.5, 0.4, 0.83])
 table_size = np.array([1.2, 1.0, 0.02])
 table_color = (0.2, 0.2, 0.8, 0.5)
-
 backwall_size = np.array([1.0, 0.01, 2.0])
-backwall_pos = np.array([0.5, 0.02, 1.0])
 backwall_color = (0.2, 0.2, 0.8, 0.5)
 
+# GEPETTO VIEWER POSITIONS
+# table_stl_pos = np.array([0.0, 0.0, -0.02])
+# table_pos = np.array([0.5, 0.4, 0.83])
+# backwall_pos = np.array([0.5, 0.02, 1.0])
+
+# MESHCAT POSITIONS
+table_stl_pos = np.array([-0.5, -0.35, -1.77])
+table_pos = np.array([0, 0.05, -0.92])
+backwall_pos = np.array([0., -0.33, -0.75])
+
 def display_disi_lab(simu):
+    # display table and stuff
+    addViewerBox(simu.robot.viz, "world/table_box", table_size[0], table_size[1], table_size[2], table_color)
+    applyViewerConfiguration(simu.robot.viz, "world/table_box", table_pos.tolist()+[0, 0, 0, 1])
+    
+    # if(simu.gui.addMesh('world/table', TABLE_STL_FILE)):
+    #     print("table added")
+    #     simu.gui.setScale('world/table', (0.001,)*3) # scale table stl because it's expressed in mm
+    #     simu.gui.callVoidProperty('world/table', 'ApplyScale')
+    # simu.robot.applyConfiguration("world/table", table_stl_pos.tolist()+[0, 0, 0, 1])
+    
+    addViewerBox(simu.robot.viz, "world/backwall", backwall_size[0], backwall_size[1], backwall_size[2], backwall_color)
+    applyViewerConfiguration(simu.robot.viz, "world/backwall", backwall_pos.tolist()+[0, 0, 0, 1])
+    
+
+def display_disi_lab_gepetto(simu):
     # display table and stuff
     simu.gui.addBox("world/table_box", table_size[0], table_size[1], table_size[2], table_color)
     simu.robot.applyConfiguration("world/table_box", table_pos.tolist()+[0, 0, 0, 1])
