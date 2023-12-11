cb65eb505174b46f4395a43551a9e667f41de674
andreadelprete <andre.delprete@gmail.com>
2023-12-11 23:21:31 +0100

    Add backward pass of BoxDDP

--- optimal_control/solutions/ddp_sol_prof.py
+++ optimal_control/solutions/ddp_sol_prof.py
@@ -98,3 +98,76 @@ def backward_pass(solver, X_bar, U_bar, mu):
             # V_xx[i,:]   = solver.Q_xx[i,:] - solver.Q_xu[i,:,:] @ Qbar_uu_pinv @ solver.Q_xu[i,:,:].T
                     
         return (solver.w, solver.K)
+
+
+def backward_pass_box(solver, X_bar, U_bar, mu, u_min, u_max):
+        ''' Backward pass of box DDP, which is DDP with control bounds. '''
+        n = X_bar.shape[1]
+        m = U_bar.shape[1]
+        N = U_bar.shape[0]
+  
+        # the Value function is defined by a quadratic function: 0.5 x' V_{xx,i} x + V_{x,i} x
+        V_xx = np.zeros((N+1, n, n))
+        V_x  = np.zeros((N+1, n))
+        
+        # dynamics derivatives w.r.t. x and u
+        A = np.zeros((N, n, n))
+        B = np.zeros((N, n, m))
+        
+        # initialize value function
+        solver.l_x[-1,:]  = solver.cost_final_x(X_bar[-1,:])
+        solver.l_xx[-1,:,:] = solver.cost_final_xx(X_bar[-1,:])
+        V_xx[N,:,:] = solver.l_xx[N,:,:]
+        V_x[N,:]    = solver.l_x[N,:]
+        
+        for i in range(N-1, -1, -1):
+            if(solver.DEBUG):
+                print("\n *** Time step %d ***" % i)
+                
+            # compute dynamics Jacobians
+            A[i,:,:] = solver.f_x(X_bar[i,:], U_bar[i,:])
+            B[i,:,:] = solver.f_u(X_bar[i,:], U_bar[i,:])
+                
+            # compute the gradient of the cost function at X=X_bar
+            solver.l_x[i,:]    = solver.cost_running_x(i, X_bar[i,:], U_bar[i,:])
+            solver.l_xx[i,:,:] = solver.cost_running_xx(i, X_bar[i,:], U_bar[i,:])
+            solver.l_u[i,:]    = solver.cost_running_u(i, X_bar[i,:], U_bar[i,:])
+            solver.l_uu[i,:,:] = solver.cost_running_uu(i, X_bar[i,:], U_bar[i,:])
+            solver.l_xu[i,:,:] = solver.cost_running_xu(i, X_bar[i,:], U_bar[i,:])
+            
+            # compute regularized cost-to-go
+            solver.Q_x[i,:]     = solver.l_x[i,:] + A[i,:,:].T @ V_x[i+1,:]
+            solver.Q_u[i,:]     = solver.l_u[i,:] + B[i,:,:].T @ V_x[i+1,:]
+            solver.Q_xx[i,:,:]  = solver.l_xx[i,:,:] + A[i,:,:].T @ V_xx[i+1,:,:] @ A[i,:,:]
+            solver.Q_uu[i,:,:]  = solver.l_uu[i,:,:] + B[i,:,:].T @ V_xx[i+1,:,:] @ B[i,:,:]
+            solver.Q_xu[i,:,:]  = solver.l_xu[i,:,:] + A[i,:,:].T @ V_xx[i+1,:,:] @ B[i,:,:]
+            
+            # account for directions in which u is saturated
+            i_bool = np.logical_and(U_bar[i,:]<u_max, U_bar[i,:]>u_min)
+            m_free = int(np.sum(i_bool)) # number of free control inputs
+            print("Max |u| = ", np.max(np.abs(U_bar[i])))
+            print("Number of saturated control inputs: ", m-m_free)
+            i_free = np.where(i_bool)[0] # indeces of the free control inputs
+            Q_uu = solver.Q_uu[i,:,:]
+            # create a reduced Q_uu considering only the non-saturated (free) control inputs
+            Q_uu_free       = Q_uu[i_bool,:][:,i_bool] + mu*np.identity(m_free)
+            Q_uu_free_inv   = np.linalg.pinv(Q_uu_free)
+            # map Q_uu_free_inv to Q_uu_inv
+            Q_uu_inv = np.zeros((m,m))
+            for j in range(m_free):
+                for k in range(m_free):
+                    Q_uu_inv[i_free[j], i_free[k]] = Q_uu_free_inv[j,k]
+            solver.w[i,:]       = - Q_uu_inv @ solver.Q_u[i,:]
+            solver.K[i,:,:]     = - Q_uu_inv @ solver.Q_xu[i,:,:].T
+                
+            # update Value function
+            V_x[i,:]    = (solver.Q_x[i,:] + 
+                solver.K[i,:,:].T @ solver.Q_u[i,:] +
+                solver.K[i,:,:].T @ solver.Q_uu[i,:,:] @ solver.w[i,:] +
+                solver.Q_xu[i,:,:] @ solver.w[i,:])
+            V_xx[i,:]   = (solver.Q_xx[i,:,:] + 
+                solver.K[i,:,:].T @ solver.Q_uu[i,:,:] @ solver.K[i,:,:] + 
+                solver.Q_xu[i,:,:] @ solver.K[i,:,:] + 
+                solver.K[i,:,:].T @ solver.Q_xu[i,:,:].T)
+                    
+        return (solver.w, solver.K)
