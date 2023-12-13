ed97ed16e0350ff82b553cf069b4dab88b38ea01
andreadelprete <andre.delprete@gmail.com>
2023-12-13 08:37:06 +0100

    [LIPM] Add cop bounds and save data to file.

--- lipm/lipm_ocp.py
+++ lipm/lipm_ocp.py
@@ -9,7 +9,8 @@ class LipmOcp:
         self.N = N
         self.w = np.sqrt(9.81/com_height)
 
-    def solve(self, x_init, wc, wdc, wu, c_ref, dc_ref, u_ref):
+
+    def solve(self, x_init, wc, wdc, wu, c_ref, dc_ref, u_ref, u_max):
         self.opti = casadi.Opti()
         N = self.N
         self.x = self.opti.variable(2, N+1)
@@ -30,10 +31,15 @@ class LipmOcp:
 
         ch = cosh(w*self.dt)
         sh = sinh(w*self.dt)
+        A = casadi.numpy.array([[ch, sh/w],
+                                [w*sh, ch]])
+        B = casadi.numpy.array([[1-ch],
+                                [w*sh]])
         for i in range(N):
             self.opti.subject_to( x[0,i+1] == ch*x[0,i] + sh*x[1,i]/w + (1-ch)*u[i] )
             self.opti.subject_to( x[1,i+1] == w*sh*x[0,i] + ch*x[1,i] - w*sh*u[i])
-            # self.opti.subject_to( self.opti.bounded(self.u_min, u[i], self.u_max) )
+            # self.opti.subject_to( x[:,i+1] == A @ x[:,i] + B * u[i])
+            self.opti.subject_to( self.opti.bounded(u_ref[i]-u_max, u[i], u_ref[i]+u_max) )
         self.opti.subject_to(x[:,0]==x_init)
 
         self.opti.subject_to(x[0,N]==u_ref[N-1])    # final CoM position must be on CoP ref
@@ -50,55 +56,60 @@ if __name__=="__main__":
     import matplotlib.pyplot as plt
     from reference_trajectories import manual_foot_placement, create_CoP_trajectory
     import orc.utils.plot_utils
-
-    # cost weights in the objective function:
-    wu        = 1e-1   # CoP error squared cost weight
-    wc        = 0      # CoM position error squared cost weight
-    wdc       = 1e-3   # CoM velocity error squared cost weight
-
-    # Inverted pendulum parameters:
-    h           = 0.80   # fixed CoM height (assuming walking on a flat terrain)
-    foot_length = 0.20   # foot size in the x-direction
-    foot_width  = 0.10   # foot size in the y-direciton
+    import romeo_conf as conf
 
     # MPC Parameters:
-    dt              = 0.1                         # sampling time interval
-    step_time       = 0.8                         # time needed for every step
-    N_1_step        = int(round(step_time/dt))    # number of time steps for a footstep
-
-    # walking parameters:
-    step_length     = 0.21                  # fixed step length in the xz-plane
-    N_steps         = 10                     # number of desired walking steps
-    N               = N_steps * N_1_step    # number of desired walking intervals
+    foot_length = conf.lxn + conf.lxp  # foot size in the x-direction
+    foot_width = conf.lyn + conf.lyp  # foot size in the y-direciton
+    nb_dt_per_step = int(round(conf.T_step / conf.dt_mpc))
+    N = conf.nb_steps * nb_dt_per_step  # number of desired walking intervals
 
     # CoM initial state:
-    x_0 = np.array([0.0, 0.0])
-    y_0 = np.array([-0.09, 0.0])
-    foot_step_0   = np.array([0.0, -0.09])   # initial foot step position in x-y
+    x_0 = np.array([conf.foot_step_0[0], 0.0])
+    y_0 = np.array([conf.foot_step_0[1], 0.0])
 
     # compute Com/CoP reference trajectories:
-    # step_width = 2*np.absolute(y_0[0])
-    desired_footsteps  = manual_foot_placement(foot_step_0, step_length, N_steps)
-    U_ref = create_CoP_trajectory(N_steps, desired_footsteps, N, N_1_step)
+    foot_steps  = manual_foot_placement(conf.foot_step_0, conf.step_length, 
+                                               conf.nb_steps)
+    foot_steps[1:, 0] -= conf.step_length
+    U_ref = create_CoP_trajectory(conf.nb_steps, foot_steps, N, nb_dt_per_step)
     C_ref = np.zeros(N+1) # not used
-    DC_ref = np.tile(step_length/step_time, N+1)
+    DC_ref = np.tile(conf.step_length/conf.T_step, N+1)
 
-    ocp = LipmOcp(dt, N, h)
-    sol_x = ocp.solve(x_0, wc, wdc, wu, C_ref, DC_ref, U_ref[:,0])
+    ocp = LipmOcp(conf.dt_mpc, N, conf.h)
+
+    sol_x = ocp.solve(x_0, conf.wc, conf.wdc, conf.wu, C_ref, DC_ref, U_ref[:,0], foot_length/2)
+    com_state_x = sol_x.value(ocp.x)
+    cop_x = sol_x.value(ocp.u)
+
+    sol_y = ocp.solve(y_0, conf.wc, conf.wdc, conf.wu, C_ref, 0*DC_ref, U_ref[:,1], foot_width/2)
+    com_state_y = sol_y.value(ocp.x)
+    cop_y = sol_y.value(ocp.u)
 
     plt.figure()
-    plt.plot(sol_x.value(ocp.x[0,:]), label="CoM X pos")
-    plt.plot(sol_x.value(ocp.x[1,:]), label="CoM X vel")
-    plt.plot(sol_x.value(ocp.u),      label="CoP X")
-    plt.plot(U_ref[:,0], ':', label="foot steps X")
+    plt.plot(com_state_x[0,:],  label="CoM X pos")
+    plt.plot(com_state_x[1,:],  label="CoM X vel")
+    plt.plot(cop_x,             label="CoP X")
+    plt.plot(U_ref[:,0], ':',   label="foot steps X")
     plt.legend()
 
-    sol_y = ocp.solve(y_0, wc, wdc, wu, C_ref, 0*DC_ref, U_ref[:,1])
     plt.figure()
-    plt.plot(sol_y.value(ocp.x[0,:]), label="CoM Y pos")
-    plt.plot(sol_y.value(ocp.x[1,:]), label="CoM Y vel")
-    plt.plot(sol_y.value(ocp.u),      label="CoP Y")
-    plt.plot(U_ref[:,1], ':', label="foot steps Y")
+    plt.plot(com_state_y[0,:],  label="CoM Y pos")
+    plt.plot(com_state_y[1,:],  label="CoM Y vel")
+    plt.plot(cop_y,             label="CoP Y")
+    plt.plot(U_ref[:,1], ':',   label="foot steps Y")
     plt.legend()
+
+    np.savez(
+        conf.DATA_FILE_LIPM,
+        com_state_x=com_state_x.T,
+        com_state_y=com_state_y.T,
+        cop_ref=U_ref,
+        cop_x=cop_x,
+        cop_y=cop_y,
+        foot_steps=foot_steps,
+    )
 
     plt.show()
+
+
